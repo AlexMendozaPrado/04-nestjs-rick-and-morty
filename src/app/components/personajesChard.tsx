5334ef5798826ce808ea5def550bc3a464042911
AlexMendozaPrado <a00819383@tec.mx>
2024-04-28 16:22:44 -0600

    Componentes favorito y tarjeta de personaje

--- src/app/components/personajesChard.tsx
+++ src/app/components/personajesChard.tsx
@@ -1,2 +1,53 @@
 import Image from "next/image";
-import React,{useContext} from "react";
+import Link from "next/link";
+import React, { useContext } from "react";
+import { ContextoFavorito } from "../context-personajes/contexto-personaje";
+import { RickAndMortyCharactersInfo } from "../types-ts/rick-and-morty-characters-info";
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+
+interface personajesChardProps {
+    personaje: RickAndMortyCharactersInfo;
+}
+
+export function PersonajeCard({ personaje }: personajesChardProps) {
+    // Contexto de favoritos
+    const { agregarPersonajeFavorito, removerPersonajeFavorito, verificarexistenciaPersonaje } = useContext(ContextoFavorito);
+
+    // Función para manejar el agregar y remover personajes
+    const manejarAgregarRemoverPersonaje = (personaje: RickAndMortyCharactersInfo) => {
+        if (verificarexistenciaPersonaje(personaje.id) >= 0) {
+            removerPersonajeFavorito(personaje.id);
+        } else {
+            agregarPersonajeFavorito(personaje);
+        }
+    }
+
+    return (
+        <Link href={`/personajes/${personaje.id}`} passHref>
+            <div className="max-w-sm cursor-pointer overflow-hidden shadow-lg">
+                <div className="relative w-full h-72">
+                    <Image
+                        loader={() => personaje.image}
+                        src={personaje.image}
+                        unoptimized
+                        width={300}
+                        height={300}
+                        alt={personaje.name}
+                    />
+                </div>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{personaje.name}</div>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={(event) => {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        manejarAgregarRemoverPersonaje(personaje);
+                    }}>
+                        {verificarexistenciaPersonaje(personaje.id) >= 0 ? <AiFillStar /> : <AiOutlineStar />}
+                    </button>
+                </div>
+            </div>
+        </Link>
+    );
+}
--- src/app/context-personajes/contexto-personaje.tsx
+++ src/app/context-personajes/contexto-personaje.tsx
@@ -3,20 +3,23 @@ import { useState,useEffect,createContext,ReactNode } from "react";
 //Type  de personaje
 import { RickAndMortyCharactersInfo } from "../types-ts/rick-and-morty-characters-info"; 
 
+//Contexto de favoritos
 interface ContextoinfoFavoritos{
     tema:boolean
     personajesFavoritos: RickAndMortyCharactersInfo[];
     agregarPersonajeFavorito: (personaje: RickAndMortyCharactersInfo) => void;
-    verificarexistenciaPersonaje: (personaje: number) => boolean;
+    verificarexistenciaPersonaje: (personaje: number) => number;
     removerPersonajeFavorito: (personajeId: number) => void;
     cambiarTema: () => void;
 
 }
+//Props de favoritos
 interface ContextoinfoFavoritosProviderProps{  
     hijo: ReactNode;
 }
+//Contexto de favoritos
 export const ContextoFavorito= createContext({} as ContextoinfoFavoritos);
-
+//Provider de favoritos
 export function ContextoFavoritosProvider({hijo}:ContextoinfoFavoritosProviderProps){
     const [personajesFavoritos,setpersonajesFavoritos]=useState<RickAndMortyCharactersInfo[]>([]);
     const [tema,setTema]=useState<boolean>(true);
@@ -24,7 +27,44 @@ export function ContextoFavoritosProvider({hijo}:ContextoinfoFavoritosProviderPr
     function cambiarTema(){
         setTema(!tema);
     }
-    
+    function agregarPersonajeFavorito(personaje: RickAndMortyCharactersInfo){
+        const verificarExistencia=verificarexistenciaPersonaje(personaje.id);
+        if (verificarExistencia<0){
+            setpersonajesFavoritos((state)=>[...state,personaje])
+            localStorage.setItem('favoritos',JSON.stringify([...personajesFavoritos,personaje]))
 
+
+        }
+    }
+    function removerPersonajeFavorito(personajeId: number){
+        const nuevoListado=personajesFavoritos.filter((personaje)=>personaje.id!==personajeId);
+        localStorage.setItem('favoritos',JSON.stringify(nuevoListado))
+        return nuevoListado;
+    }
+    function verificarexistenciaPersonaje(personajeId: number){
+        return personajesFavoritos.findIndex((personaje)=>personaje.id===personajeId);
+    }
+    //useEffect para guardar en localstorage
+useEffect(()=>{
+const personajesFavoritosGuardados=localStorage.getItem('favoritos');
+if(!personajesFavoritosGuardados){
+return
 }
+setpersonajesFavoritos(JSON.parse(personajesFavoritosGuardados));
+},[])
+
+    return(
+        <ContextoFavorito.Provider value={{
+            tema,
+            personajesFavoritos,
+            agregarPersonajeFavorito,
+            verificarexistenciaPersonaje,
+            removerPersonajeFavorito,
+            cambiarTema
+        }}>
+            {hijo}
+        </ContextoFavorito.Provider>
+    )
+}
+
 
